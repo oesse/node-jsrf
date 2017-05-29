7bcd2c777b86ede925dc647b0caf1068d48d5c08
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2017-05-29 23:23:48 +0200

    Change command line interface to enable expand object.

--- src/cli.js
+++ src/cli.js
@@ -1,20 +1,44 @@
-import { extractVariableFromRange } from './'
+import { extractVariableFromRange, expandObject } from './'
 
-export default function () {
+function readFromStdin () {
   let data = ''
   process.stdin.setEncoding('utf-8')
 
-  process.stdin.on('readable', function () {
-    let chunk = process.stdin.read()
-    while (chunk) {
-      data += chunk
-      chunk = process.stdin.read()
-    }
-  })
+  return new Promise((resolve, reject) => {
+    process.stdin.on('readable', function () {
+      let chunk = process.stdin.read()
+      while (chunk) {
+        data += chunk
+        chunk = process.stdin.read()
+      }
+    })
 
-  process.stdin.on('end', function () {
-    const [,, start, end, varName] = process.argv
-    const diffs = extractVariableFromRange(data, [start, end], varName)
-    console.log(JSON.stringify(diffs))
+    process.stdin.on('end', function () {
+      resolve(data)
+    })
   })
+}
+
+export default async function () {
+  const argv = process.argv.slice(2)
+  const [mode, start, end] = argv
+
+  if (mode === 'extract-variable') {
+    const varName = argv[3]
+    const sourceCode = await readFromStdin()
+
+    const diffs = extractVariableFromRange(sourceCode, [start, end], varName)
+    console.log(JSON.stringify(diffs))
+    return
+  }
+
+  if (mode === 'expand-object') {
+    const sourceCode = await readFromStdin()
+
+    const diff = expandObject(sourceCode, [start, end])
+    console.log(JSON.stringify([diff]))
+    return
+  }
+
+  console.log('Error: Invalid mode')
 }
