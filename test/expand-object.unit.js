ef05666e3c5bbf6077e68b2a7d11ef0b7ad45161
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2017-06-02 09:45:49 +0200

    Restructure tests to be more readable.

--- test/expand-object.unit.js
+++ test/expand-object.unit.js
@@ -13,32 +13,23 @@ describe('expandObject', () => {
     expect(diff).to.have.property('code')
   })
 
-  describe('change set', () => {
-    it('defines the range (lines, columns) of the object to be expanded', () => {
-      const sourceCode = 'const foo = { a: \'a\' }'
-      const charRange = [12, 22]
-      const diff = expandObject(sourceCode, charRange)
+  it('puts the property key and value of a single line object literal on its own line', () => {
+    const sourceCode = 'const foo = { a: \'a\' }'
+    const charRange = [12, 22]
+    const diff = expandObject(sourceCode, charRange)
 
-      expect(diff).to.have.property('line')
-        .to.eql([1, 1])
-      expect(diff).to.have.property('column')
-        .to.eql([12, 22])
-    })
+    expect(diff).to.have.property('line')
+      .to.eql([1, 1])
+    expect(diff).to.have.property('column')
+      .to.eql([12, 22])
+    expect(diff).to.have.property('code', '{\n  a: \'a\'\n}')
+  })
 
-    it('has object property on a single line', () => {
-      const sourceCode = 'const foo = { a: \'a\' }'
-      const charRange = [12, 22]
-      const diff = expandObject(sourceCode, charRange)
+  it('puts multiple properties each on a line of their own', () => {
+    const sourceCode = 'const foo = { a: \'a\', b: \'b\' }'
+    const charRange = [12, 22]
+    const diff = expandObject(sourceCode, charRange)
 
-      expect(diff).to.have.property('code', '{\n  a: \'a\'\n}')
-    })
-
-    it('puts multiple properties each on a line of their own', () => {
-      const sourceCode = 'const foo = { a: \'a\', b: \'b\' }'
-      const charRange = [12, 22]
-      const diff = expandObject(sourceCode, charRange)
-
-      expect(diff).to.have.property('code', '{\n  a: \'a\',\n  b: \'b\'\n}')
-    })
+    expect(diff).to.have.property('code', '{\n  a: \'a\',\n  b: \'b\'\n}')
   })
 })
--- test/extract-variable.unit.js
+++ test/extract-variable.unit.js
@@ -3,47 +3,54 @@ import { expect } from 'chai'
 import { extractVariable } from '../src/extract-variable'
 
 describe('extractVariable', () => {
-  context('with range inside function parameter', () => {
+  it('returns two changes each with line and column range and replacement code', () => {
+    const sourceCode = 'doImportantStuff(1, a + b)'
+    const charRange = [20, 25]
+    const changes = extractVariable(sourceCode, charRange)
+
+    expect(changes).to.have.length(2)
+    changes.forEach(change => {
+      expect(change).to.have.property('line')
+      expect(change).to.have.property('column')
+      expect(change).to.have.property('code')
+    })
+  })
+
+  it('extracts expression into const variable', () => {
     const sourceCode = 'doImportantStuff(1, a + b)'
     const charRange = [20, 25]
 
-    it('returns change sets defining const variable with expression from range', () => {
-      const [ diff1, diff2 ] = extractVariable(sourceCode, charRange, 'varName')
+    const [ diff1, diff2 ] = extractVariable(sourceCode, charRange, 'varName')
 
-      expect(diff1).to.eql({ line: [1, 1], column: [20, 25], code: 'varName' })
-      expect(diff2).to.eql({ line: [1, 1], column: [0, 0], code: 'const varName = a + b\n' })
-    })
+    expect(diff1).to.eql({ line: [1, 1], column: [20, 25], code: 'varName' })
+    expect(diff2).to.eql({ line: [1, 1], column: [0, 0], code: 'const varName = a + b\n' })
   })
 
-  context('with expression inside function', () => {
+  it('defines extracted variable in nearest enclosing scope', () => {
     const sourceCode = 'function hello () {\n  doImportantStuff(1, a + b)\n}'
     const charRange = [42, 47]
 
-    it('puts variable declaration just before usage', () => {
-      const [ diff1, diff2 ] = extractVariable(sourceCode, charRange, 'varName')
+    const [ diff1, diff2 ] = extractVariable(sourceCode, charRange, 'varName')
 
-      expect(diff1).to.eql({
-        line: [2, 2],
-        column: [22, 27],
-        code: 'varName'
-      })
-      expect(diff2).to.eql({ line: [2, 2], column: [2, 2], code: 'const varName = a + b\n  ' })
+    expect(diff1).to.eql({
+      line: [2, 2],
+      column: [22, 27],
+      code: 'varName'
     })
+    expect(diff2).to.eql({ line: [2, 2], column: [2, 2], code: 'const varName = a + b\n  ' })
   })
 
-  context('with expression inside async function', () => {
+  it('works inside async functions', () => {
     const sourceCode = 'async function hello () {\n  await doImportantStuff(1, a + b)\n}'
     const charRange = [54, 59]
 
-    it('puts variable declaration just before usage', () => {
-      const [ diff1, diff2 ] = extractVariable(sourceCode, charRange, 'varName')
+    const [ diff1, diff2 ] = extractVariable(sourceCode, charRange, 'varName')
 
-      expect(diff1).to.eql({
-        line: [2, 2],
-        column: [28, 33],
-        code: 'varName'
-      })
-      expect(diff2).to.eql({ line: [2, 2], column: [2, 2], code: 'const varName = a + b\n  ' })
+    expect(diff1).to.eql({
+      line: [2, 2],
+      column: [28, 33],
+      code: 'varName'
     })
+    expect(diff2).to.eql({ line: [2, 2], column: [2, 2], code: 'const varName = a + b\n  ' })
   })
 })
