2930c9a94e06565397179da2ff78599554c40676
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2017-06-06 10:34:11 +0200

    Restructure tests.

--- test/expand.unit.js
+++ test/expand.unit.js
@@ -3,27 +3,25 @@ import { expect } from 'chai'
 import { expand } from '../src/expand'
 
 describe('expand', () => {
+  it('returns change set with line and column range and replacement code', () => {
+    const sourceCode = 'const foo = { a: \'a\' }'
+    const charRange = [12, 22]
+    const diff = expand(sourceCode, charRange)
+
+    expect(diff).to.have.property('line')
+    expect(diff).to.have.property('column')
+    expect(diff).to.have.property('code')
+  })
+
   context('an object literal', () => {
-    it('returns change set with line and column range and replacement code', () => {
-      const sourceCode = 'const foo = { a: \'a\' }'
-      const charRange = [12, 22]
-      const diff = expand(sourceCode, charRange)
-
-      expect(diff).to.have.property('line')
-      expect(diff).to.have.property('column')
-      expect(diff).to.have.property('code')
-    })
-
     it('puts the property key and value of a single line object literal on its own line', () => {
       const sourceCode = 'const foo = { a: \'a\' }'
       const charRange = [12, 22]
       const diff = expand(sourceCode, charRange)
 
-      expect(diff).to.have.property('line')
-        .to.eql([1, 1])
-      expect(diff).to.have.property('column')
-        .to.eql([12, 22])
-      expect(diff).to.have.property('code', '{\n  a: \'a\'\n}')
+      expect(diff.line).to.eql([1, 1])
+      expect(diff.column).to.eql([12, 22])
+      expect(diff.code).to.eql('{\n  a: \'a\'\n}')
     })
 
     it('puts multiple properties each on a line of their own', () => {
@@ -31,7 +29,7 @@ describe('expand', () => {
       const charRange = [12, 22]
       const diff = expand(sourceCode, charRange)
 
-      expect(diff).to.have.property('code', '{\n  a: \'a\',\n  b: \'b\'\n}')
+      expect(diff.code).to.eql('{\n  a: \'a\',\n  b: \'b\'\n}')
     })
 
     it('expands closest enclosing object literal', () => {
@@ -39,7 +37,7 @@ describe('expand', () => {
       const charRange = [17, 17]
       const diff = expand(sourceCode, charRange)
 
-      expect(diff).to.have.property('code', '{\n  a: \'a\',\n  b: \'b\'\n}')
+      expect(diff.code).to.eql('{\n  a: \'a\',\n  b: \'b\'\n}')
     })
   })
 
@@ -52,6 +50,16 @@ describe('expand', () => {
       expect(diff.line).to.eql([1, 1])
       expect(diff.column).to.eql([12, 17])
       expect(diff.code).to.eql('[\n  a\n]')
+    })
+
+    it('puts multiple array elements each on a line of their own', () => {
+      const sourceCode = 'const foo = [ a, b ]'
+      const charRange = [12, 12]
+      const diff = expand(sourceCode, charRange)
+
+      expect(diff.line).to.eql([1, 1])
+      expect(diff.column).to.eql([12, 20])
+      expect(diff.code).to.eql('[\n  a,\n  b\n]')
     })
   })
 })
