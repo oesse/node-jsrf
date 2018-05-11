8500b8e02aa662b63f47fda5fd253a73d47e9cb9
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2018-05-11 17:36:44 +0200

    fix: allow for jsx spread attributes to be parsed

--- src/walk.js
+++ src/walk.js
@@ -4,14 +4,15 @@ const JsxContinuations = {
   JSXAttribute: (node, st, c) => node.argument && c(node.argument.value, st, 'Expression'),
   JSXExpressionContainer: (node, st, c) => c(node.expression, st, 'Expression'),
   JSXEmptyExpression: () => {},
+  JSXSpreadAttribute: (node, st, c) => c(node.argument, st, 'Expression'),
   JSXElement: (node, st, c) => {
     for (const attribute of node.openingElement.attributes) {
-      c(attribute, st, 'JSXAttribute') 
+      c(attribute, st, attribute.type)
     }
-    for (const child of node.children) { 
+    for (const child of node.children) {
       if (child.type !== 'JSXText') { c(child, st, 'Expression') }
     }
-  },
+  }
 }
 
 export default (ast, visitor) => {
@@ -22,7 +23,7 @@ export default (ast, visitor) => {
     {
       ...walk.base,
       ...JsxContinuations,
-      SpreadProperty: (node, st, c) => c(node.argument, st, 'Expression'),
+      SpreadProperty: (node, st, c) => c(node.argument, st, 'Expression')
     },
     walkingState
   )
---
+++ test/integration/expand/jsx-rest-property.itest.js
@@ -0,0 +1,10 @@
+// Given: a jsx element with spread property attribute
+const C = (props) => <Forward {...props} />
+
+// Do
+expand([22, 22])
+
+// Expect: the jsx tag to be expanded
+const C = (props) => <Forward 
+  {...props}
+/>
